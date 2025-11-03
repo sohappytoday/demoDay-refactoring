666e79b1076c6751ef2b6cf6c911d090604784ca
geoKim <kgeo6264@gmail.com>
2025-11-03 14:34:30 +0900

    feat: Implement DepartureDto in message package

---
+++ src/main/java/com/lamarfishing/core/log/message/dto/request/DepartureRequest.java
@@ -0,0 +1,7 @@
+package com.lamarfishing.core.log.message.dto.request;
+
+import com.lamarfishing.core.schedule.domain.Schedule;
+
+public class DepartureRequest {
+    private Schedule.Status scheduleStatus;
+}
---
+++ src/main/java/com/lamarfishing/core/log/message/dto/response/DepartureResponse.java
@@ -0,0 +1,9 @@
+package com.lamarfishing.core.log.message.dto.response;
+
+import java.time.LocalDateTime;
+
+public class DepartureResponse {
+    private LocalDateTime timeStamp;
+    private String recipientPhone;
+    private String content;
+}
