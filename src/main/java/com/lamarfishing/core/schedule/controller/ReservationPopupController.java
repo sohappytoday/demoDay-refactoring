793ecba292eab2030d183ea0d5c98a7ae158f633
geoKim <kgeo6264@gmail.com>
2025-11-04 18:42:33 +0900

    feat: Add grade parameter in ReservationPopupController

--- src/main/java/com/lamarfishing/core/schedule/controller/ReservationPopupController.java
+++ src/main/java/com/lamarfishing/core/schedule/controller/ReservationPopupController.java
@@ -17,10 +17,11 @@ public class ReservationPopupController {
      * 토큰 받는 방식 수정 필요
      */
     @GetMapping
-    public ResponseEntity<ApiResponse<ReservationPopupResponse>> getReservationPopup(@RequestAttribute(name = "수정필요") Long userId,
+    public ResponseEntity<ApiResponse<ReservationPopupResponse>> getReservationPopup(@RequestAttribute(name = "수정필요1") Long userId,
+                                                                                    @RequestAttribute(name = "수정필요2") String grade,
                                                                                     @PathVariable("schedule_public_id") String publicId){
 
-        ReservationPopupResponse popupResponse = reservationPopupService.getReservationPopup(userId,publicId);
+        ReservationPopupResponse popupResponse = reservationPopupService.getReservationPopup(userId,grade,publicId);
 
         return ResponseEntity.ok(ApiResponse.success("예약 팝업 조회에 성공하였습니다",popupResponse));
     }
