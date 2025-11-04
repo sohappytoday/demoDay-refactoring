b26a80d4430ad62d70ab0e47c5e7ee25882dfceb
geoKim <kgeo6264@gmail.com>
2025-11-04 15:09:26 +0900

    feat: Implement temporary ReservationPopupController

---
+++ src/main/java/com/lamarfishing/core/schedule/controller/ReservationPopupController.java
@@ -0,0 +1,28 @@
+package com.lamarfishing.core.schedule.controller;
+
+import com.lamarfishing.core.common.ApiResponse;
+import com.lamarfishing.core.schedule.dto.response.ReservationPopupResponse;
+import com.lamarfishing.core.schedule.service.ReservationPopupService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/schedules/{schedule_public_id}/reservation")
+public class ReservationPopupController {
+
+    private final ReservationPopupService reservationPopupService;
+    /**
+     * 토큰 받는 방식 수정 필요
+     */
+    @GetMapping
+    public ResponseEntity<ApiResponse<ReservationPopupResponse> getReservationPopup(@RequestAttribute(name = "수정필요") Long userId,
+                                                                                    @PathVariable("schedule_public_id") String publicId){
+
+        ReservationPopupResponse popupResponse = reservationPopupService.getReservationPopup(userId,publicId);
+
+        return ResponseEntity.ok(ApiResponse.success("예약 팝업 조회에 성공하였습니다",popupResponse));
+    }
+
+}
