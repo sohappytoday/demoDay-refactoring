a96bff19030f7802552fc1c87c5c529026f9a467
geoKim <kgeo6264@gmail.com>
2025-11-06 21:20:48 +0900

    fix: Add User grade and User nickname where toReservationCommonDto

--- src/main/java/com/lamarfishing/core/reservation/mapper/ReservationMapper.java
+++ src/main/java/com/lamarfishing/core/reservation/mapper/ReservationMapper.java
@@ -3,12 +3,18 @@ package com.lamarfishing.core.reservation.mapper;
 import com.lamarfishing.core.reservation.domain.Reservation;
 import com.lamarfishing.core.reservation.dto.command.ReservationCommonDto;
 import com.lamarfishing.core.schedule.dto.response.ReservationCreateResponse;
+import com.lamarfishing.core.user.domain.User;
 
 public class ReservationMapper {
 
     public static ReservationCommonDto toReservationCommonDto(Reservation reservation) {
+        User.Grade grade = reservation.getUser().getGrade();
+        String nickname = reservation.getUser().getNickname();
+
         return ReservationCommonDto.builder()
                 .reservationPublicId(reservation.getPublicId())
+                .nickname(nickname)
+                .grade(grade)
                 .headCount(reservation.getHeadCount())
                 .process(reservation.getProcess())
                 .build();
