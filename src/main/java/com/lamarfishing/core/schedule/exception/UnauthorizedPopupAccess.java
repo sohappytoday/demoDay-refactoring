1a8348b7cd1207e8d6dca835d391631d241bdb90
geooeg <kgeo6264@gmail.com>
2025-11-17 19:44:18 +0900

    fix: Fix UnauthorizedPopupAccess exception

--- src/main/java/com/lamarfishing/core/schedule/exception/UnauthorizedPopupAccess.java
+++ src/main/java/com/lamarfishing/core/schedule/exception/UnauthorizedPopupAccess.java
@@ -1,9 +1,10 @@
 package com.lamarfishing.core.schedule.exception;
 
 import com.lamarfishing.core.common.exception.BusinessException;
+import com.lamarfishing.core.common.exception.ErrorCode;
 
 public class UnauthorizedPopupAccess extends BusinessException {
     public UnauthorizedPopupAccess() {
-        super(ErrorCode.);
+        super(ErrorCode.UNAUTHORIZED_SCHEDULE_ACCESS);
     }
 }
