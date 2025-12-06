b3a42f23e9ddaad07ce9632b0a420768f70190f8
geoKim <kgeo6264@gmail.com>
2025-12-07 04:17:07 +0900

    refactor: Remove unused method in UserRepository

--- src/main/java/com/lamarfishing/core/user/repository/UserRepository.java
+++ src/main/java/com/lamarfishing/core/user/repository/UserRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    public User findByPhone(String phone);
     Optional<User> findById(long id);
     Optional<User> findByProviderAndSub(Provider provider, String sub);
 }
